c23e9582ef4e9011182dbd49fe38f0f2601123e2
28JayG <jaygandhi.32@outlook.com>
2024-11-24 23:09:34 +0530

    web app development page responsive

--- src/components/layouts/WebAppDevelopmentServices.tsx
+++ src/components/layouts/WebAppDevelopmentServices.tsx
@@ -1,51 +1,38 @@
-import React from 'react'
-import MaxWidthWrapper from './MaxWidthWrapper'
-import TitleSummary from 'components/shared/TitleSummary'
-import Button from 'components/shared/Button'
+import React from "react";
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import TitleSummary from "components/shared/TitleSummary";
+import Button from "components/shared/Button";
+import ServiceCard from "components/shared/ServiceCard";
 
 const WebAppDevelopmentServices: React.FC = () => {
   return (
-    <div className="bg-gray-100 mt-10">
-      <MaxWidthWrapper className="py-20">
+    <div className="bg-gray-100 py-10 mt-10">
+      <MaxWidthWrapper>
         <TitleSummary
           title="Our Web App Development Services"
-          summary="Explore our mobile application development services for the modern enterprise. Our app development expertise helps create apps that are both intuitive & revenue generating.  "
+          summary="Explore our web application development services for the modern enterprise. Our web app development expertise helps create web apps that are both engaging and latency free."
         />
-        <div className="flex gap-5 my-10">
-          <div className="flex flex-col gap-5 p-8 w-[645px] rounded-2xl bg-white">
-            <div className="bg-gray-100 p-2 w-fit rounded-2xl">
-              <img src="/SVG/hybrid_app.svg" alt="" className="w-10 h-10" />
-            </div>
-            <p className="font-bold text-2xl">
-              Cross Platform mobile apps development
-            </p>
-            <p className="text-gray-600">
-              Maximise your reach with a single codebase. Our cross-platform
-              solutions ensure seamless performance across iOS and Android,
-              reducing development time and costs. 
-            </p>
-            <div>
-            <Button color="black">Talk To Our Experts</Button>
-            </div>
-          </div>
-          <div className="flex flex-col gap-5 p-8 w-[645px] rounded-2xl bg-white">
-            <div className="bg-gray-100 p-2 w-fit rounded-2xl">
-              <img src="/SVG/hybrid_app.svg" alt="" className="w-10 h-10" />
-            </div>
-            <p className="font-bold text-2xl">Native mobile apps development</p>
-            <p className="text-gray-600">
-              Unleash full power of device-specific features. Our native app
-              development services for iOS and Android, ensures the highest
-              level of app functionality and user satisfaction. 
-            </p>
-            <div>
-            <Button color="black">Talk To Our Experts</Button>
-            </div>
-          </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 mt-10">
+          <ServiceCard
+            illustrationUri="/SVG/hybrid_app.svg"
+            title="Cross Platform mobile apps development"
+            description="Maximise your reach with a single codebase. Our cross-platform
+            solutions ensure seamless performance across iOS and Android,
+            reducing development time and costs."
+            cta={<Button color="black">Talk To Our Experts</Button>}
+          />
+          <ServiceCard
+            illustrationUri="/SVG/hybrid_app.svg"
+            title=" Native mobile apps development"
+            description="Unleash full power of device-specific features. Our native app
+            development services for iOS and Android, ensures the highest
+            level of app functionality and user satisfaction."
+            cta={<Button color="black">Talk To Our Experts</Button>}
+          />
         </div>
       </MaxWidthWrapper>
     </div>
-  )
-}
+  );
+};
 
-export default WebAppDevelopmentServices
+export default WebAppDevelopmentServices;
--- src/pages/web-app-development.tsx
+++ src/pages/web-app-development.tsx
@@ -1,9 +1,12 @@
-import ContactForm from "components/shared/ContactForm";
 import MaxWidthWrapper from "components/layouts/MaxWidthWrapper";
 import PageLayout from "components/layouts/PageLayout";
-import SuccessStories from "components/mobile-development/SuccessStories";
 import WebAppDevelopmentServices from "components/layouts/WebAppDevelopmentServices";
+import SuccessStories from "components/mobile-development/SuccessStories";
+import About from "components/shared/About";
 import Button from "components/shared/Button";
+import CompanyContactCard from "components/shared/Cards/CompanyContactCard";
+import TopClientsCard from "components/shared/Cards/TopClientsCard";
+import ContactForm from "components/shared/ContactForm";
 import FrequentlyAskedQues from "components/shared/FrequentlyAskedQues";
 import Hero from "components/shared/Hero";
 import RoundedCard from "components/shared/RoundedCard";
@@ -11,93 +14,34 @@ import TechStack from "components/shared/TechStack";
 import TitleSummary from "components/shared/TitleSummary";
 import { questionsArrayWebApp } from "data/FAQ.data";
 import React from "react";
-import { FaSkype } from "react-icons/fa";
 import { IoArrowForwardCircleOutline } from "react-icons/io5";
-import { LuMail, LuPhone } from "react-icons/lu";
-import { COMPANY_PHONE_1 } from "constants/company.constants";
 
 const WebAppDevelopment: React.FC = () => {
   return (
     <PageLayout>
       <Hero
-        title="services for feature rich, no latency & speediest web apps. "
+        title="services for feature rich, no latency & speediest web apps."
         subTitle="At Propelius, we latest trends in web application development to develop web apps that are designed to provide interactive, dynamic experiences right in your browser without the need for downloading software. Our apps are ideal for businesses looking to offer services, manage data, or connect with users across any device."
       />
-      <RoundedCard
-        children={
-          <div className="flex gap-8 items-center justify-center font-semibold text-3xl">
-            <p className="flex gap-2 items-center">
-              <LuPhone />
-              {COMPANY_PHONE_1}
-            </p>
-            <p className="flex gap-2 items-center">
-              <LuMail />
-              info@propelius.tech
-            </p>
-            <p className="flex gap-2 items-center">
-              <FaSkype />
-              tech.propelius
-            </p>
-          </div>
-        }
-      />
-      <MaxWidthWrapper>
-        <div className="flex justify-between mt-20">
-          <p className="w-[400px] font-semibold text-4xl">
-            Operate more efficiently and reach their audience wherever they are.
-          </p>
-          <p className="leading-relaxed w-[620px]">
-            With the vast majority of consumers spending significant time on
-            their smartphones, a web app puts your business directly into their
-            hands, offering convenience and accessibility like never before.
-            <br />
-            We are  one of the best web application development companies in
-            India. Our apps offer valuable data insights, helping you understand
-            user behaviour and refine your offerings. They also create new
-            revenue streams through in-app purchases or ads. 
-          </p>
-        </div>
-      </MaxWidthWrapper>
+      <CompanyContactCard />
       <div className="my-20">
-        <RoundedCard
-          variant="bordered"
-          children={
-            <div>
-              <p className="text-lg md:text-xl font-semibold md:font-bold">
-                Tailored Digital Solution For Leading Enterprises
-              </p>
-              <div className="flex justify-around items-center gap-5">
-                <img
-                  src="/images/client_img1.png"
-                  alt="clientImg1"
-                  className="w-20 md:w-52 h-12 md:h-28"
-                />
-                <img
-                  src="/images/client_img2.png"
-                  alt="clientImg2"
-                  className="w-20 md:w-52 h-12 md:h-28"
-                />
-                <img
-                  src="/images/client_img3.png"
-                  alt="clientImg3"
-                  className="w-20 md:w-52 h-12 md:h-28"
-                />
-                <img
-                  src="/images/client_img4.png"
-                  alt="clientImg4"
-                  className="w-20 md:w-52 h-12 md:h-28"
-                />
-                <img
-                  src="/images/client_img5.png"
-                  alt="clientImg5"
-                  className="w-20 md:w-52 h-12 md:h-28"
-                />
-              </div>
-            </div>
-          }
+        <About
+          title="Operate more efficiently and reach their audience wherever they are."
+          description="
+          With the vast majority of consumers spending significant time on
+          their smartphones, a web app puts your business directly into their
+          hands, offering convenience and accessibility like never before.
+          We are  one of the best web application development companies in
+          India. Our apps offer valuable data insights, helping you understand
+          user behaviour and refine your offerings. They also create new
+          revenue streams through in-app purchases or ads. 
+          "
         />
       </div>
       <MaxWidthWrapper className="mb-20">
+        <div className="my-20">
+          <TopClientsCard />
+        </div>
         <TitleSummary
           title="Our Success Stories"
           summary="Explore unique client cases and check on the potential solutions that we can offer through our mobile app development success stories. "
@@ -111,21 +55,23 @@ const WebAppDevelopment: React.FC = () => {
       <SuccessStories />
       <WebAppDevelopmentServices />
       <TechStack />
-      <RoundedCard
-        variant="bordered"
-        children={
-          <TitleSummary
-            title="Ready to Bring Your App Idea to Life?"
-            summary="Innovation is just a conversation away. Reach out and let's
-              discuss your app aspirations"
-            action={
-              <Button endIcon={<IoArrowForwardCircleOutline size={25} />}>
-                Contact Us
-              </Button>
-            }
-          />
-        }
-      />
+      <MaxWidthWrapper className="mt-10 mb-20">
+        <RoundedCard
+          variant="bordered"
+          children={
+            <TitleSummary
+              title="Ready to Bring Your App Idea to Life?"
+              summary="Innovation is just a conversation away. Reach out and let's
+          discuss your app aspirations"
+              action={
+                <Button endIcon={<IoArrowForwardCircleOutline size={25} />}>
+                  Contact Us
+                </Button>
+              }
+            />
+          }
+        />
+      </MaxWidthWrapper>
       <FrequentlyAskedQues questions={questionsArrayWebApp} />
       <ContactForm />
     </PageLayout>
