e25e5a60656ae408814649114ee05dbe8e6411ac
28JayG <jaygandhi.32@outlook.com>
2024-11-24 16:45:36 +0530

    about us responsive

--- src/components/shared/CarouseButtons.tsx
+++ src/components/shared/CarouseButtons.tsx
@@ -1,23 +1,31 @@
+import classNames from "classnames";
 import { FC } from "react";
 import { IoChevronBack, IoChevronForward } from "react-icons/io5";
 
 interface ButtonProps {
   onClick?: () => void;
+  className?: string;
 }
 
-export const PrevButton: FC<ButtonProps> = ({ onClick }) => (
+export const PrevButton: FC<ButtonProps> = ({ onClick, className }) => (
   <div
     onClick={onClick}
-    className="bg-grey-700 bg-opacity-5 rounded-full w-12 h-12 flex items-center justify-center"
+    className={classNames(
+      "bg-grey-700 bg-opacity-5 rounded-full w-12 h-12 flex items-center justify-center",
+      className
+    )}
   >
     <IoChevronBack size={24} />
   </div>
 );
 
-export const NextButton: FC<ButtonProps> = ({ onClick }) => (
+export const NextButton: FC<ButtonProps> = ({ onClick, className }) => (
   <div
     onClick={onClick}
-    className="bg-grey-700 bg-opacity-5 rounded-full w-12 h-12 flex items-center justify-center"
+    className={classNames(
+      "bg-grey-700 bg-opacity-5 rounded-full w-12 h-12 flex items-center justify-center",
+      className
+    )}
   >
     <IoChevronForward size={24} />
   </div>
--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -6,7 +6,7 @@ import Hero from "components/shared/Hero";
 import RoundedCard from "components/shared/RoundedCard";
 import TitleSummary from "components/shared/TitleSummary";
 import { divide } from "lodash";
-import React from "react";
+import React, { useState } from "react";
 import {
   FaChevronCircleLeft,
   FaChevronCircleRight,
@@ -21,8 +21,37 @@ import { LuMail, LuPhone } from "react-icons/lu";
 import About from "components/shared/About";
 import { aboutUsDescription, aboutUsTitle } from "constants/about.constants";
 import { COMPANY_PHONE_1 } from "constants/company.constants";
+import { NextButton, PrevButton } from "components/shared/CarouseButtons";
 
-const aboutUs: React.FC = () => {
+const cultureGlipmseImages = [
+  "/images/our_culture_img1.png",
+  "/images/our_culture_img2.png",
+  "/images/our_culture_img3.png",
+];
+
+const AboutUs: React.FC = () => {
+  const [selectedImage, setSelectedImage] = useState<string>(
+    cultureGlipmseImages[0]
+  );
+
+  const handleNext = () => {
+    const currentIndex = cultureGlipmseImages.findIndex(
+      (item) => item === selectedImage
+    );
+    const nextIndex = (currentIndex + 1) % cultureGlipmseImages.length; // Wrap around to the start
+    setSelectedImage(cultureGlipmseImages[nextIndex]);
+  };
+
+  const handlePrev = () => {
+    const currentIndex = cultureGlipmseImages.findIndex(
+      (item) => item === selectedImage
+    );
+    const prevIndex =
+      (currentIndex - 1 + cultureGlipmseImages.length) %
+      cultureGlipmseImages.length; // Wrap around to the end
+    setSelectedImage(cultureGlipmseImages[prevIndex]);
+  };
+
   return (
     <PageLayout>
       <Hero
@@ -45,18 +74,18 @@ const aboutUs: React.FC = () => {
       />
       <RoundedCard
         children={
-          <div className="flex gap-8 items-center justify-center font-semibold text-3xl">
+          <div className="flex gap-8 items-center justify-center text-green-900 font-semibold text-2xl">
             <p className="flex gap-2 items-center">
               <LuPhone />
-              {COMPANY_PHONE_1}
+              <p className="hidden lg:block">{COMPANY_PHONE_1}</p>
             </p>
             <p className="flex gap-2 items-center">
               <LuMail />
-              info@propelius.tech
+              <p className="hidden lg:block">info@propelius.tech</p>
             </p>
             <p className="flex gap-2 items-center">
               <FaSkype />
-              tech.propelius
+              <p className="hidden lg:block">tech.propelius</p>
             </p>
           </div>
         }
@@ -64,18 +93,19 @@ const aboutUs: React.FC = () => {
 
       <MaxWidthWrapper>
         <About title={aboutUsTitle} description={aboutUsDescription} />
-        <div className="flex justify-center items-center">
-          <p className="text-[260px] outline-text">We</p>
-          <ul className="font-semibold text-4xl p-2 space-y-3 text-green-dark">
+
+        <div className="flex flex-col lg:flex-row justify-center items-start lg:items-center">
+          <p className="text-[212px] lg:text-[260px] outline-text">We</p>
+          <ul className="font-semibold text-2xl lg:text-4xl p-2 space-y-3 text-green-900">
             <li>Drive Innovation.</li>
             <li>Deliver Results.</li>
             <li>Transform Ideas.</li>
             <li>Create Solutions.</li>
           </ul>
         </div>
-        <div className="py-24 flex justify-center gap-32">
+        <div className="py-24 flex flex-col lg:flex-row justify-center gap-32">
           <div className="flex flex-col max-w-[530px] gap-3">
-            <p className="font-light text-textSecondary">
+            <p className="font-light text-grey-700">
               Propelius: Propelling you into the digital age{" "}
             </p>
             <p className="font-semibold text-4xl text-green-dark">
@@ -88,12 +118,14 @@ const aboutUs: React.FC = () => {
               seasoned professionals united, frustrated by the disconnect they
               saw in the industry.
               <br />
+              <br />
               At the heart of this group was Yash, our esteemed founder and CEO.
               A Software Engineer and Technical Lead with degrees from BITS and
               Santa Clara University, Yash brought invaluable expertise in
               FinTech and SaaS. His experience, gained from over 6 years of
               building web and mobile applications with Silicon Valley startups,
               shaped our problem-solving approach.
+              <br />
               <br />
               Yash's deep understanding of business challenges in evolving tech
               landscapes became the cornerstone of Propelius. Today, we stand as
@@ -106,71 +138,75 @@ const aboutUs: React.FC = () => {
             <img
               src="/images/brand_story_img.png"
               alt=""
-              className="w-[650px] h-[760px]"
+              className="w-full lg:max-w-[650px] h-80 lg:h-[760px]"
             />
           </div>
         </div>
       </MaxWidthWrapper>
-      <div className="bg-primary h-[612px] w-full relative mb-[305px] ">
-        <MaxWidthWrapper className="">
-          <div className="pt-24 flex flex-col gap-3 ">
+      <div className="relative w-full pb-20">
+        <div className="bg-green-500 w-full h-3/4 top-0 absolute" />
+        <MaxWidthWrapper className="relative overflow-hidden">
+          <div className="flex flex-col gap-3 pt-20 pb-10 lg:pb-20">
             <p className="text-white font-light">Our Culture</p>
-            <div className="flex justify-between items-center">
+            <div className="flex flex-col lg:flex-row justify-between items-start lg:items-center">
               <p className="font-semibold text-4xl text-white leading-relaxed">
                 A Glimpse into Our Culture
               </p>
-              <div className="flex gap-4">
-                <div className="bg-green-400 rounded-full p-2 text-white items-center">
-                  <IoChevronBack size={20} />
-                </div>
-                <div className="bg-green-400 rounded-full p-2 text-white items-center">
-                  <IoChevronForward size={20} />
-                </div>
+              <div className="flex gap-4 lg:hidden">
+                <PrevButton className="text-white" />
+                <NextButton className="text-white" />
               </div>
             </div>
-            <p className="text-white font-light">
+            <p className="text-white font-light max-w-[640px]">
               At Propelius, our team thrives on creativity, embraces diversity,
               and celebrates each other's successes. We're not just colleagues;
               we're a family of tech enthusiasts working together to shape the
               digital future.
             </p>
           </div>
-          <div className="flex gap-5 absolute right-0 bottom-0 translate-y-1/2">
+          <div className="hidden lg:flex gap-5">
+            {cultureGlipmseImages.map((img) => (
+              <img
+                key={img}
+                src={img}
+                alt="our_culture_img1"
+                className="w-[646px] h-[404px]"
+              />
+            ))}
+          </div>
+          <div className="flex flex-col lg:hidden gap-7">
             <img
-              src="/images/our_culture_img1.png"
+              src={selectedImage}
               alt="our_culture_img1"
-              className="w-[646px] h-[404px]"
+              className="w-full h-[404px]"
             />
-            <img
-              src="/images/our_culture_img2.png"
-              alt="our_culture_img2"
-              className="w-[314px] h-[404px]"
-            />
-            <img
-              src="/images/our_culture_img3.png"
-              alt="our_culture_img3"
-              className="w-[498px] h-[404px]"
-            />
+            <div className="flex gap-3">
+              <PrevButton onClick={handlePrev} />
+              <NextButton onClick={handleNext} />
+            </div>
           </div>
         </MaxWidthWrapper>
       </div>
+
       <RoundedCard
         variant="bordered"
         children={
-          <div className="flex flex-col gap-5">
-            <div className="flex justify-between">
-              <p className="max-w-[635px] text-primary text-4xl font-semibold text-left">
+          <div className="flex flex-col lg:flex-row lg:items-center justify-between gap-5 px-3">
+            <div className="flex flex-col gap-3 justify-between">
+              <p className="w-full lg:max-w-[635px] text-center lg:text-left text-green-500 text-2xl lg:text-3xl font-semibold">
                 Fuel Your Business with Propelius' Tech Expertise - Book a free
                 30 mins discovery call
               </p>
-              <Button endIcon={<IoArrowForwardCircleOutline size={25} />}>
+              <p className="lg:max-w-[600px] text-center lg:text-left text-green-900">
+                Mould the perfect digital solution for your business with our
+                expert team - start shaping your online identity
+              </p>
+            </div>
+            <div className="w-64 mx-auto" >
+              <Button className="w-full" endIcon={<IoArrowForwardCircleOutline size={25} />}>
                 Book A Free Call
               </Button>
             </div>
-            <p className="max-w-[600px] text-left">
-              Mould the perfect digital solution for your business with our
-              expert team - start shaping your online identity
-            </p>
           </div>
         }
       />
@@ -179,4 +215,4 @@ const aboutUs: React.FC = () => {
   );
 };
 
-export default aboutUs;
+export default AboutUs;
